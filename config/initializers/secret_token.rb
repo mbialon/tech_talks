616fe1ba04a5cdc872d42a46576fc00e1f0bc2ba
Marcin Białoń <marcin.bialon@gmail.com>
2010-12-08 21:08:16 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TechTalks::Application.config.secret_token = '5728414651d867c82d187603b5420537416f56a3795ac361ce960914e5895627772ba0e87afa9f575b77bd106180ae6298715182e7d6ab20dd56a2e3f5579065'
