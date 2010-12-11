53a12b0e742c13a5e1f4cf358802271072e70308
Marcin Białoń <marcin.bialon@gmail.com>
2010-12-11 21:17:42 +0100

    update and destroy talks

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,8 +1,12 @@
 class TalksController < ApplicationController
-  before_filter :authenticate_user!, :except => [:index]
+  before_filter :authenticate_user!, :except => [:index, :show]
 
   def index
     @talks = Talk.find :all
+  end
+  
+  def show
+    @talk = Talk.find(params[:id])
   end
 
   def new
@@ -12,7 +16,30 @@ class TalksController < ApplicationController
   def create
     @talk = Talk.new(params[:talk])
     @talk.user = current_user
-    @talk.save
+    if @talk.save
+      redirect_to talk_path(@talk)
+    else
+      render :action => :new
+    end
+  end
+
+  def edit
+    @talk = Talk.find(params[:id])
+  end
+  
+  def update
+    @talk = Talk.find(params[:id])
+    @talk.update_attributes(params[:talk])
+    if @talk.save
+      redirect_to talk_path(@talk)
+    else
+      render :action => :edit
+    end
+  end
+
+  def destroy
+    @talk = Talk.find(params[:id])
+    @talk.destroy
     redirect_to talks_path
   end
 end
--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,3 +1,6 @@
 class Talk < ActiveRecord::Base
+  validates_presence_of :title
+  validates_presence_of :abstract
+
   belongs_to :user
 end
